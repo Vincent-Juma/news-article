0821b61db7c55ca50db4d6a817d9766dec3114c3
Vincent-Juma <vincentjuma64@gmail.com>
2021-02-22 12:08:29 +0300

    Initial commits

---
+++ instance/config.py
@@ -0,0 +1,13 @@
+NEWS_API_KEY = '3c82ea4131dd41b0b777eee004edca9c'
+
+# from flask import Flask
+# from .config import DevConfig
+
+# # Initializing application
+# app = Flask(__name__,instance_relative_config = True)
+
+# # Setting up configuration
+# app.config.from_object(DevConfig)
+# app.config.from_pyfile('config.py')
+
+# from app import views
