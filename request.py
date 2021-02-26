151ded45d41732ebf4646b6099d191046d0c1292
Vincent-Juma <vincentjuma64@gmail.com>
2021-02-26 16:52:47 +0300

    create request.py

--- request.py
+++ request.py
@@ -5,15 +5,15 @@ def get_movie(id):
         movie_details_data = url.read()
         movie_details_response = json.loads(movie_details_data)
 
-        movie_object = None
-        if movie_details_response:
-            id = movie_details_response.get('id')
-            title = movie_details_response.get('original_title')
-            overview = movie_details_response.get('overview')
-            poster = movie_details_response.get('poster_path')
-            vote_average = movie_details_response.get('vote_average')
-            vote_count = movie_details_response.get('vote_count')
+        news_object = None
+        if news_details_response:
+            id = news_details_response.get('id')
+            title = news_details_response.get('original_title')
+            overview = news_details_response.get('overview')
+            poster = news_details_response.get('poster_path')
+            vote_average = news_details_response.get('vote_average')
+            vote_count = news_details_response.get('vote_count')
 
-            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+            news_object = Movie(id,title,overview,poster,vote_average,vote_count)
 
-    return movie_object
+    return news_object
