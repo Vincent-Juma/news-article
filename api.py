00a98dc31043ad134da8999c5c82c0cec39b937a
Vincent-Juma <vincentjuma64@gmail.com>
2021-03-01 22:25:09 +0300

    delete api file

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
-from newsapi import NewsApiClient
-from instance import news_api_key
-import datetime as dt
-import pandas as pd
+# from newsapi import NewsApiClient
+# from instance import news_api_key
+# import datetime as dt
+# import pandas as pd
 
 
-newsapi = NewsApiClient(api_key=news_api_key)
+# newsapi = NewsApiClient(api_key=news_api_key)
