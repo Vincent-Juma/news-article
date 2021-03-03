073a82b4c8f579f6e7f2bb73a498779834d1fb80
Vincent-Juma <vincentjuma64@gmail.com>
2021-03-04 01:07:05 +0300

    trash unused layout.html files

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,37 @@
+import os
 
+class Config:
+  '''
+  general configuration parent class
+  '''
+
+  NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+  SECRET_KEY = os.environ.get('SECRET_KEY')
+
+  pass
+
+class ProdConfig(Config):
+  '''
+  production configuration child class
+  Args:
+    Config: The Parent configuration class with general configuration settings
+  '''
+
+  pass
+
+class DevConfig(Config):
+  '''
+  development configuration child class
+  Args:
+    Config: The Parent configuration class with general configuration settings
+  '''
+
+  DEBUG = True
+
+config_options = {
+  'development': DevConfig,
+  'production': ProdConfig
+}
 # # from flask import Flask
 # # from .config import DevConfig
 
